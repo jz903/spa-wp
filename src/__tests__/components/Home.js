0d52d849effcd221a74cef366c67c5cfbec2e656
Jason Zhou <jasonzhou903@qq.com>
2017-09-11 22:54:29 +0800

    add husky back and fix test

--- src/__tests__/components/Home.js
+++ src/__tests__/components/Home.js
@@ -6,8 +6,13 @@ import { MemoryRouter } from 'react-router-dom'
 import Home from '../../components/Home'
 
 const initProps = {
+  pageId: 0,
+  page: {},
   posts: {},
+  homeCarousel: [],
+  pageSection: [],
   fetchPosts: jest.fn(),
+  fetchSinglePage: jest.fn(),
 }
 const setup = (props = initProps) => {
   const component = <Home {...props} />
@@ -35,7 +40,7 @@ describe('Home component', () => {
   it('should render correct things', () => {
     const { wrapper } = setup()
 
-    expect(wrapper.find('.App').length === 1).toBe(true)
+    expect(wrapper.find('.app-home').length === 1).toBe(true)
   })
 
   it("should render right thing and don't change unexpected", () => {
